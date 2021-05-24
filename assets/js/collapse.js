0d9f25c57a128cfebc83c99daf3bf606da5eea2b
unknown <ies.nalindapubudu@gmail.com>
2021-05-24 09:37:38 -0800

    Style Modified

---
+++ assets/js/collapse.js
@@ -0,0 +1,74 @@
+$(function() {
+    $("#btn_collapse1").click(function() {
+        var i = $(this).find('i');
+        if ($("#demo1").is(':visible')) {
+            i.removeClass('fa fa-angle-down').addClass('fa fa-angle-right');
+        } else {
+            i.removeClass('fa fa-angle-right').addClass('fa fa-angle-down');
+        }
+        $("#demo1").toggle();
+    });
+    $("#btn_collapse2").click(function() {
+        var i = $(this).find('i');
+        if ($("#demo2").is(':visible')) {
+            i.removeClass('fa fa-angle-down').addClass('fa fa-angle-right');
+        } else {
+            i.removeClass('fa fa-angle-right').addClass('fa fa-angle-down');
+        }
+        $("#demo2").toggle();
+    });
+    $("#btn_collapse3").click(function() {
+        var i = $(this).find('i');
+        if ($("#demo3").is(':visible')) {
+            i.removeClass('fa fa-angle-down').addClass('fa fa-angle-right');
+        } else {
+            i.removeClass('fa fa-angle-right').addClass('fa fa-angle-down');
+        }
+        $("#demo3").toggle();
+    });
+    $("#btn_collapse4").click(function() {
+        var i = $(this).find('i');
+        if ($("#demo4").is(':visible')) {
+            i.removeClass('fa fa-angle-down').addClass('fa fa-angle-right');
+        } else {
+            i.removeClass('fa fa-angle-right').addClass('fa fa-angle-down');
+        }
+        $("#demo4").toggle();
+    });
+    $("#btn_collapse5").click(function() {
+        var i = $(this).find('i');
+        if ($("#demo5").is(':visible')) {
+            i.removeClass('fa fa-angle-down').addClass('fa fa-angle-right');
+        } else {
+            i.removeClass('fa fa-angle-right').addClass('fa fa-angle-down');
+        }
+        $("#demo5").toggle();
+    });
+    $("#btn_collapse6").click(function() {
+        var i = $(this).find('i');
+        if ($("#demo6").is(':visible')) {
+            i.removeClass('fa fa-angle-down').addClass('fa fa-angle-right');
+        } else {
+            i.removeClass('fa fa-angle-right').addClass('fa fa-angle-down');
+        }
+        $("#demo6").toggle();
+    });
+    $("#btn_collapse7").click(function() {
+        var i = $(this).find('i');
+        if ($("#demo7").is(':visible')) {
+            i.removeClass('fa fa-angle-down').addClass('fa fa-angle-right');
+        } else {
+            i.removeClass('fa fa-angle-right').addClass('fa fa-angle-down');
+        }
+        $("#demo7").toggle();
+    });
+    $("#btn_collapse8").click(function() {
+        var i = $(this).find('i');
+        if ($("#demo8").is(':visible')) {
+            i.removeClass('fa fa-angle-down').addClass('fa fa-angle-right');
+        } else {
+            i.removeClass('fa fa-angle-right').addClass('fa fa-angle-down');
+        }
+        $("#demo8").toggle();
+    });
+});
